d04f58b1ad4501392b207490578dee1b57d1e346
NetPuter <netputer@gmail.com>
2014-10-07 18:04:01 +0800

    feat: First blood

---
+++ index.js
@@ -0,0 +1,19 @@
+/* global require, module */
+var injector = require('connect-injector');
+
+module.exports = function (options) {
+    options = options || {};
+    options.host = options.host || '127.0.0.1';
+    options.port = options.port || '8080';
+    options.id = options.id || 'anonymous';
+
+    return injector(function (req, res) {
+        return res.getHeader('content-type').indexOf('text/html') > -1;
+    }, function (content, req, res, callback) {
+        var scriptSrc = 'http://' + options.host + ':' + options.port + '/target/target-script-min.js#' + options.id;
+
+        content = content.toString().replace('</head>', '<script src="' + scriptSrc + '"></script></head>');
+
+        callback(null, content);
+    });
+};
