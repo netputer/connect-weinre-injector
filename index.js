701702134a3e53274aef6b8a6c09ab46be182b98
NetPuter <netputer@gmail.com>
2014-10-12 17:07:37 +0800

    chore: Better fallback for response header

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function (options) {
     options.id = options.id || 'anonymous';
 
     return injector(function (req, res) {
-        return (res.getHeader('content-type') || []).indexOf('text/html') > -1;
+        return (res.getHeader('content-type') || '').indexOf('text/html') > -1;
     }, function (content, req, res, callback) {
         var scriptSrc = 'http://' + options.host + ':' + options.port + '/target/target-script-min.js#' + options.id;
 
